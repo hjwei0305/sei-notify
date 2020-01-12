daa1ebd992e75adee1b17cf4464b47f13f6ab325
王锦光 <wangjg@changhong.com>
2020-01-12 20:48:13 +0800

    Feign测试API调用模块：api-test

--- api-test/src/main/java/com/changhong/sei/notify/apitest/NotifyApiTestApplication.java
+++ api-test/src/main/java/com/changhong/sei/notify/apitest/NotifyApiTestApplication.java
@@ -1,7 +1,8 @@
-package com.changhong.sei.notify;
+package com.changhong.sei.notify.apitest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <strong>实现功能:</strong>
@@ -10,9 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author 王锦光 wangj
  * @version 1.0.1 2019-12-18 10:41
  */
+@EnableFeignClients(basePackages = "com.changhong.sei.notify.api")
 @SpringBootApplication
-public class NotifyRestApplication {
+public class NotifyApiTestApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotifyRestApplication.class, args);
+        SpringApplication.run(NotifyApiTestApplication.class, args);
     }
 }
---
+++ api-test/src/test/java/com/changhong/sei/notify/apitest/HelloApiTest.java
@@ -0,0 +1,33 @@
+package com.changhong.sei.notify.apitest;
+
+import com.changhong.com.sei.core.test.BaseUnitTest;
+import com.changhong.sei.core.dto.ResultData;
+import com.changhong.sei.core.util.JsonUtils;
+import com.changhong.sei.notify.api.HelloService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * <strong>实现功能:</strong>
+ * <p>Feign测试API调用</p>
+ *
+ * @author 王锦光 wangj
+ * @version 1.0.1 2020-01-12 20:00
+ */
+public class HelloApiTest extends BaseUnitTest {
+    @Autowired
+    private HelloService helloService;
+
+    @Test
+    public void say(){
+        System.out.println("Feign测试API调用:say");
+    }
+
+    @Test
+    public void syHello(){
+        ResultData<String> result = helloService.sayHello("wangjg");
+        System.out.println(JsonUtils.toJson(result));
+        Assert.assertTrue(result.isSuccessful());
+    }
+}
