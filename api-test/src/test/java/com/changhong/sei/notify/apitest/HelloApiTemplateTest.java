bcb148feb2b3596bcd21fbd65e87a5444fa63a7e
王锦光 <wangjg@changhong.com>
2020-01-13 10:58:06 +0800

    HelloApiTemplateTest

---
+++ api-test/src/test/java/com/changhong/sei/notify/apitest/HelloApiTemplateTest.java
@@ -0,0 +1,52 @@
+package com.changhong.sei.notify.apitest;
+
+import com.changhong.com.sei.core.test.BaseUnitTest;
+import com.changhong.sei.apitemplate.ApiTemplate;
+import com.changhong.sei.core.dto.ResultData;
+import com.changhong.sei.core.util.JsonUtils;
+import com.changhong.sei.notify.api.HelloService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * <strong>实现功能:</strong>
+ * <p>Feign测试API模板调用</p>
+ *
+ * @author 王锦光 wangj
+ * @version 1.0.1 2020-01-12 20:00
+ */
+public class HelloApiTemplateTest extends BaseUnitTest {
+    @Autowired
+    private ApiTemplate apiTemplate;
+
+    @Test
+    public void sayHello(){
+        String uri = "http://10.4.208.86:20001/sei-notify/hello/sayHello";
+        Map<String,String> params = new LinkedHashMap<>();
+        params.put("name", "wangjg");
+        ResultData result = apiTemplate.getByUrl(uri, ResultData.class, params);
+        System.out.println(JsonUtils.toJson(result));
+        Assert.assertTrue(result.isSuccessful());
+    }
+
+    @Test
+    public void sayHelloByAppName(){
+        String appname = "sei-notify";
+        Map<String,String> params = new LinkedHashMap<>();
+        params.put("name", "wangjg");
+        ResultData result = apiTemplate.getByAppModuleCode(appname, "hello/sayHello", ResultData.class, params);
+        System.out.println(JsonUtils.toJson(result));
+        Assert.assertTrue(result.isSuccessful());
+    }
+
+    @Test
+    public void health(){
+        String uri = "http://10.4.208.86:20001/sei-notify/actuator/health";
+        String result = apiTemplate.getByUrl(uri, String.class);
+        System.out.println(JsonUtils.toJson(result));
+    }
+}
