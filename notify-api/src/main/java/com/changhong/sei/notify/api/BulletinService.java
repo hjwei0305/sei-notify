7cc9eb31956bedea2bf2cf83fe96c13bbdb2e227
王锦光 <wangjg@changhong.com>
2020-01-15 16:25:38 +0800

    维护消息通告API接口 方法notes

--- notify-api/src/main/java/com/changhong/sei/notify/api/BulletinService.java
+++ notify-api/src/main/java/com/changhong/sei/notify/api/BulletinService.java
@@ -29,7 +29,7 @@ public interface BulletinService extends FindByPageService<BulletinDto> {
      * @return 操作结果
      */
     @PostMapping(path = "saveBulletin", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ApiOperation(value = "保存通告", tags = "维护一个消息通告")
+    @ApiOperation(value = "保存通告", notes = "维护一个消息通告")
     ResultData saveBulletin(@RequestBody BulletinDto bulletinDto);
 
     /**
@@ -38,7 +38,7 @@ public interface BulletinService extends FindByPageService<BulletinDto> {
      * @return 业务处理结果
      */
     @PostMapping(path = "releaseBulletin", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ApiOperation(value = "发布通告", tags = "批量发布通告")
+    @ApiOperation(value = "发布通告", notes = "批量发布通告")
     ResultData releaseBulletin(@RequestBody Set<String> ids);
 
     /**
@@ -47,7 +47,7 @@ public interface BulletinService extends FindByPageService<BulletinDto> {
      * @return 业务处理结果
      */
     @PostMapping(path = "cancelBulletin", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ApiOperation(value = "撤销通告", tags = "批量撤销通告")
+    @ApiOperation(value = "撤销通告", notes = "批量撤销通告")
     ResultData cancelBulletin(@RequestBody Set<String> ids);
 
     /**
@@ -56,7 +56,7 @@ public interface BulletinService extends FindByPageService<BulletinDto> {
      * @return 业务处理结果
      */
     @PostMapping(path = "deleteBulletin", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ApiOperation(value = "删除通告", tags = "批量删除通告")
+    @ApiOperation(value = "删除通告", notes = "批量删除通告")
     ResultData deleteBulletin(@RequestBody Set<String> ids);
 
     /**
@@ -65,6 +65,6 @@ public interface BulletinService extends FindByPageService<BulletinDto> {
      * @return 获取一个通告
      */
     @GetMapping(path = "getBulletin")
-    @ApiOperation(value = "查看通告", tags = "获取一个通告")
+    @ApiOperation(value = "查看通告", notes = "获取一个通告")
     ResultData<BulletinDto> getBulletin(@RequestParam("id") String id);
 }
