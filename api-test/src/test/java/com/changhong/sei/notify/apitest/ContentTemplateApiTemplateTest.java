d1066b097b359bf207d97d296536b955d2e0e599
王锦光 <wangjg@changhong.com>
2020-01-22 21:24:20 +0800

    sei-notify-api上传Maven仓库

---
+++ api-test/src/test/java/com/changhong/sei/notify/apitest/ContentTemplateApiTemplateTest.java
@@ -0,0 +1,31 @@
+package com.changhong.sei.notify.apitest;
+
+import com.changhong.com.sei.core.test.BaseUnitTest;
+import com.changhong.sei.apitemplate.ApiTemplate;
+import com.changhong.sei.core.dto.ResultData;
+import com.changhong.sei.core.util.JsonUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 实现功能:
+ *
+ * @author 王锦光 wangjg
+ * @version 2020-01-22 20:24
+ */
+public class ContentTemplateApiTemplateTest extends BaseUnitTest {
+    @Autowired
+    private ApiTemplate apiTemplate;
+
+    @Test
+    public void findAll(){
+        String appname = "sei-notify";
+        ResultData result = apiTemplate.getByAppModuleCode(appname, "contentTemplate/findAll", ResultData.class);
+        System.out.println(JsonUtils.toJson(result));
+        Assert.assertTrue(result.isSuccessful());
+    }
+}
