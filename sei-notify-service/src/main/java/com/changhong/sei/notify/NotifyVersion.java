e037a827929cb3d775457b38e303dbd0b081b7a9
Vision.Mac <chao2.ma@changhong.com>
2021-01-08 18:22:00 +0800

    升级springboot版本

---
+++ sei-notify-service/src/main/java/com/changhong/sei/notify/NotifyVersion.java
@@ -0,0 +1,17 @@
+package com.changhong.sei.notify;
+
+import com.changhong.sei.core.context.Version;
+
+/**
+ * 应用版本
+ *
+ * @author 马超(Vision.Mac)
+ * @version 1.0.1 2018/6/21 16:29
+ */
+public final class NotifyVersion extends Version {
+
+    public NotifyVersion() {
+        super(NotifyVersion.class);
+    }
+
+}
